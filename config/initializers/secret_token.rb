ee73ce9db1b415aafb034fadd8680b0e8178f38b
Jesicka Labud <jesicka@gmail.com>
2014-01-13 08:00:57 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Drawscript::Application.config.secret_key_base = '5ba3666fdf9062286fed981d83d8ab455ef02afb7d3697cfcf447205601f21533fbb5b98cbdc0fc2dee11eef4d2fa4b570caccd52fc8b1848f21d2ccdbd5fb19'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Drawscript::Application.config.session_store :cookie_store, key: '_drawscript_session'
